106ebd47b05ae195a6dc14d22368912c47fb9411
Sumon Sinha <sumonsinha@Sumons-MacBook-Air.local>
2023-08-05 21:37:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header.jsx";
+import Meme from "./components/Meme.jsx";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
