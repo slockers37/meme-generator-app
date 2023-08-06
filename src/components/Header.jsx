b0fb8e91107706a65163119af28a332943778283
Sumon Sinha <sumonsinha@Sumons-MacBook-Air.local>
2023-08-06 11:05:02 +0530

    fixed logo not loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img src="/public/troll-face.png" className="header--image" />
+      <img src="/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">Project</h4>
     </header>
